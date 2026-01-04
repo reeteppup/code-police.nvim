16d36575f16afc8897ab9489906d4fde586a50dc
jl115 <joelbern006@gmail.com>
2026-01-04 06:06:02 +0100

    fix: updated lua

--- lua/code-police/languages/lua.lua
+++ lua/code-police/languages/lua.lua
@@ -1,29 +1,26 @@
 return {
 	func_query = [[
-		(function_declaration) @func_header
-		(function_definition) @func_header
-		(local_function_declaration) @func_header 
-	]],
+        (function_declaration parameters: (parameters) @func_header)
+        (function_definition parameters: (parameters) @func_header)
+    ]],
 	body_types = { "block" },
+
 	nesting_list = {
 		"if_statement",
 		"for_statement",
 		"while_statement",
 		"repeat_statement",
-		"do_statement",
-		"function_definition",
 	},
 	complexity_query = [[
-		(if_statement) @rule_if
-		(elseif_statement) @rule_if 
-		(for_statement) @rule_loop
-		(while_statement) @rule_loop
-		(repeat_statement) @rule_loop
-		(do_statement) @rule_loop 
-	  ; Lua uses pcall/xpcall instead of try-catch
-    ; We match the function call name to reward safety
-    ((function_call 
-      name: (identifier) @__name 
-      (#any-of? @__name "pcall" "xpcall")) @rule_try)
-	]],
+        (if_statement) @rule_if
+        (elseif_statement) @rule_if
+        (for_statement) @rule_loop
+        (while_statement) @rule_loop
+        (repeat_statement) @rule_loop
+        ; Lua uses pcall/xpcall instead of try-catch
+        ; We match the function call name to reward safety
+        ((function_call 
+            name: (identifier) @__name 
+            (#any-of? @__name "pcall" "xpcall")) @rule_try)
+    ]],
 }
